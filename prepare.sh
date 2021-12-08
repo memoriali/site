4d9791ad8a2467b5e3e445e95a2471721b8395d0
dima <dima@zaharov.info>
2021-12-08 21:29:38 +0200

    Test region added

--- prepare.sh
+++ prepare.sh
@@ -9,10 +9,10 @@ sed -i -- 's/c:\/tmp\/cache\.pdb/\/cache\/cache\.pdb/g' ./www/dataloader.py
 #
 ## The following values are just samples:
 #
-# Default Test Latvian token
-sed -i -- 's/#AUTHTOKEN#/6410c5e5-c0fb-4633-a30d-c8d70d3c53d9/g' ./www/dataloader.py
-# Default test Latvia ID
-sed -i -- 's/#COUNTRYID#/c-1/g' ./www/dataloader.py
+# Default Test Region token
+sed -i -- 's/#AUTHTOKEN#/547cfc9c-21b0-4309-a4e2-d1c945856dd9/g' ./www/dataloader.py
+# Default test Region ID
+sed -i -- 's/#COUNTRYID#/c-16/g' ./www/dataloader.py
 # Default test REST Service address
 sed -i -- 's/#RESTSERVICEADDRESS#/ciprest.memoriali.org/g' ./www/dataloader.py
 #
